0fd935a6b25fc9797e9634b7625ff3037038b21d
Sal Tamay <sal.tamay@outlook.com>
2020-02-26 23:48:58 -0500

    chore: Initial commit, project setup

---
+++ public/service-worker.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'my-site-cache-v1';
+
+var urlsToCache = [
+  '/',
+  '/styles.css',
+  '/index.js'
+];
+
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/index.html");
+        }
+      });
+    })
+  );
+});
+
