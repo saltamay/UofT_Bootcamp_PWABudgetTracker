575fc1a7b5ad647c2fdb2402d9c4cedac5501abe
Sal Tamay <sal.tamay@outlook.com>
2020-03-09 22:31:42 -0400

    feat: Add cloud db connection string

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/budget", {
+const dbConnectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost/budget';
+
+mongoose.connect(dbConnectionString, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
